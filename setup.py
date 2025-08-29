6908aca897caffe5859ed1134028f91974d0ab62
Prince Chothani <23cspri029@ldce.ac.in>
2025-08-29 16:20:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="mcqgenerator",
     version="0.0.1",
-    author="Prince Chtohani",
+    author="Prince Chothani",
     author_email="princechothani53@gmail.com",
     install_requires=["langchain", "streamlilt", "python-dotenv", "PyPDF2"],
     packages=find_packages() # this help to consider src folder as package because of __init__
